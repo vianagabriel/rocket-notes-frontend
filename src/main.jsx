3030f67bdc831f0e6232d5439d3f28aa44190802
vianagabriel <vianagabrieldacunha@gmail.com>
2022-10-19 20:45:02 -0300

    subindo página de adicionar nova nota

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import { Profile } from './pages/Profile';
+import { NewNote } from './pages/NewNote';
 
 import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
@@ -11,7 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Profile />
+      <NewNote />
     </ThemeProvider>    
   </React.StrictMode>
 )
---
+++ src/pages/NewNote/index.jsx
@@ -0,0 +1,43 @@
+import { Container, Form } from './styles';
+import { Header } from '../../components/Header';
+import { Input } from '../../components/Input';
+import { Textarea } from '../../components/Textarea';
+import { NoteItem } from '../../components/NoteItem';
+import { Section } from '../../components/Section';
+import { Button } from '../../components/Button';
+
+
+
+export function NewNote() {
+  return (
+    <Container>
+      <Header />
+
+      <main>
+        <Form>
+          <header>
+            <h1>Criar nota</h1>
+            <a href="/">voltar</a>
+          </header>
+
+          <Input placeholder='Título' />
+          <Textarea placeholder='Observações' />
+
+          <Section title='Links úteis' >
+            <NoteItem value='https://www.google.com' />
+            <NoteItem placeholder='Novo link' isNew />
+          </Section>
+
+          <Section title='Marcadores'>
+            <div className='tags'>
+              <NoteItem value='React' />
+              <NoteItem placeholder='Nova tag' isNew />
+            </div>
+          </Section>
+
+          <Button title='Salvar'/>
+        </Form>
+      </main>
+    </Container>
+  )
+} 
